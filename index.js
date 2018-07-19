e8b30bd9faf9f11fa13f7aeabc3b6472617480e7
jaig1 <njganeshv@gmail.com>
2018-07-19 14:18:03 +0000

    chuma

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Welcome to World of spinnaker!');
 });
 
 var server = app.listen(3000, function () {
